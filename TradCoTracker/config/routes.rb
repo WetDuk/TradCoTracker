7c40930275dbef2f1ce35ecd32eb9daed8f1c27d
Evelyn Rose <evelyndrose19@gmail.com>
2021-02-13 13:33:30 -0600

    adding html and css

--- TradCoTracker/config/routes.rb
+++ TradCoTracker/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root 'portal#index'
+  root 'portal#login'
+
+  get 'portal/login'
+  get 'portal/logout'
+  get 'portal/view_points'
+  get 'portal/add_points'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
