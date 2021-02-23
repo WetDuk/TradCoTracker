6df04e4f9844d6c7a9138cf8c3b3d1b86bfce893
Evelyn Rose <evelyndrose19@gmail.com>
2021-02-22 19:29:05 -0600

    added and updated more pages to website

--- TradCoTracker/config/routes.rb
+++ TradCoTracker/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'portal/logout'
   get 'portal/view_points'
   get 'portal/add_points'
+  get 'portal/create_user'
+  get 'portal/view_members'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
