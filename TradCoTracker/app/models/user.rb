52926de9362e7475b94bca4222a8f49586d25fbf
Dak.rich <dak.rich@tamu.edu>
2021-02-27 13:55:49 -0600

    dlelte

--- TradCoTracker/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ApplicationRecord
-    has_many :member_points, class_name: "member_point", foreign_key: "reference_id"
-end
--- TradCoTracker/db/migrate/20210213183537_create_org_events.rb
+++
@@ -1,8 +0,0 @@
-class CreateOrgEvents < ActiveRecord::Migration[6.1]
-  def change
-    create_table :org_events do |t|
-
-      t.timestamps
-    end
-  end
-end
