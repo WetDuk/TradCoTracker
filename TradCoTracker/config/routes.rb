d970ed3cc2577c3f6a65e47ab471268b69684eca
WetDuk <tony.qing@tamu.edu>
2021-03-12 11:31:31 -0600

    cleaning and merging routes

--- TradCoTracker/config/routes.rb
+++ TradCoTracker/config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
-=======
-  # get 'point/index'
-  # get 'point/show'
-  # get 'point/new'
-  # get 'point/edit'
-  # get 'point/delete'
->>>>>>> Stashed changes
+
   root 'portal#login'
 
   get 'portal/login'
   get 'portal/logout'
   get 'portal/view_points'
   get 'portal/add_points'
-<<<<<<< Updated upstream
   get 'portal/create_user'
   get 'portal/view_members'
-=======
   
   resources :point do
     member do
@@ -25,7 +16,6 @@ Rails.application.routes.draw do
   end
 
 
->>>>>>> Stashed changes
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
