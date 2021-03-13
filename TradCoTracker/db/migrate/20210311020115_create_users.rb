544e1dc590a9bab21b007e0b2291d2f76a86cbb9
Dak.rich <dak.rich@tamu.edu>
2021-03-13 10:23:56 -0600

    fixing data base

--- TradCoTracker/db/migrate/20210311020115_create_users.rb
+++ TradCoTracker/db/migrate/20210311020115_create_users.rb
@@ -6,6 +6,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
       t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
+      t.boolean :isOfficer
+      t.date :joinDate
     end
 
     add_index :users, :email
