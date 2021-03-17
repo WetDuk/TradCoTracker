9dab2c14a9a9f9e0276ec51b95726517c789a78e
Dak.rich <dak.rich@tamu.edu>
2021-03-17 10:26:47 -0500

    Update user.rb

--- TradCoTracker/app/models/user.rb
+++ TradCoTracker/app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   include Clearance::User
 
+    def self.search(search)
+        if search
+            where(["username LIKE ?", "%#{search}%"])
+        else
+            all
+        end
+    end
 end
