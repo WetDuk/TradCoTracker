a369a4a3a9293f19d5640026869e57ad8475e009
Adam Armendariz <armenaa652@gmail.com>
2021-03-22 01:57:10 -0500

    edit User added

--- TradCoTracker/app/controllers/users_controller.rb
+++ TradCoTracker/app/controllers/users_controller.rb
@@ -3,22 +3,32 @@
 class UsersController < ApplicationController
   # This controller is what creates the new users for the database
   def index
+    @current_user = User.find_by(id: session[:user_id])
+
     @users = User.search(params[:search])
   end
 
   def index
+    @current_user = User.find_by(id: session[:user_id])
+
     @users = User.order('id ASC')
   end
 
   def show
+    @current_user = User.find_by(id: session[:user_id])
+
     @user = User.find(params[:id])
   end
 
   def new
+    @current_user = User.find_by(id: session[:user_id])
+
     @user = User.new
   end
 
   def create
+    @current_user = User.find_by(id: session[:user_id])
+
     @user = User.new(user_params)
     # If user was able to save return to login
     if @user.save
@@ -28,16 +38,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @current_user = User.find_by(id: session[:user_id])
+
+    @user = User.new
+  end
+
   def update
+    @current_user = User.find_by(id: session[:user_id])
+
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to(user_path(@user))
+      redirect_to(portal_view_members_path)
     else
       render('edit')
     end
   end
 
   def edit
+    @current_user = User.find_by(id: session[:user_id])
+
     @user = User.find(params[:id])
   end
 
