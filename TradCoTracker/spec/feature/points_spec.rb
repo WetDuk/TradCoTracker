c06f91a46a52b8cbd67013d7b3ee24133073fdb1
Evelyn Rose <evelyndrose19@gmail.com>
2021-03-23 05:00:14 -0500

    Added rspec testing

---
+++ TradCoTracker/spec/feature/points_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+# Creating points
+RSpec.describe 'Creating points', type: :feature do
+  scenario 'with valid inputs' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    visit new_point_path
+    fill_in 'point_eventName', with: 'test event'
+    fill_in 'point_pointAmount', with: '3'
+    fill_in 'point_submissionDate', with: '03/23/2021'
+    click_on 'Add Points'
+    expect(page).to have_content('test event')
+  end
+end
+
+# deleting points
+RSpec.describe 'Deleting points', type: :feature do
+  scenario 'with valid inputs' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    visit new_point_path
+    fill_in 'point_eventName', with: 'test event'
+    fill_in 'point_pointAmount', with: '3'
+    fill_in 'point_submissionDate', with: '03/23/2021'
+    click_on 'Add Points'
+    expect(page).to have_content('test event')
+    click_on 'Delete'
+    expect(page).not_to have_content('test event')
+  end
+end
+
+# View points
+RSpec.describe 'Deleting points', type: :feature do
+  scenario 'with valid inputs' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    visit new_point_path
+    fill_in 'point_eventName', with: 'test event'
+    fill_in 'point_pointAmount', with: '3'
+    fill_in 'point_submissionDate', with: '03/23/2021'
+    click_on 'Add Points'
+    visit points_path
+    expect(page).to have_content('test event')
+  end
+end
--- TradCoTracker/spec/feature/users_spec.rb
+++ TradCoTracker/spec/feature/users_spec.rb
@@ -1,22 +1,238 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe 'users', type: :feature do
   describe 'goes to root path' do
     it 'shows the right content' do
-      visit root_path
-      expect(page).to have_content('Password')
-    end
+    visit root_path
+    expect(page).to have_content('Password')
+  end
 
-    it 'shows the right content' do
+  it 'shows the right content' do
       visit new_user_path
       expect(page).to have_content('Password')
     end
 
-    it 'shows the right content' do
+  it 'shows the right content' do
       visit portal_view_members_path
       expect(page).to have_content('Password')
     end
+  end
+end
+
+
+# Creating a user
+RSpec.describe 'Creating a user with', type: :feature do
+
+  # Sunny day cases for creating a user
+  scenario 'valid inputs for a non-officer' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    click_on 'Create Account'
+    expect(page).to have_content('Log in')
+  end
+
+  scenario 'valid inputs for an officer' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'testOfficer@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+    expect(page).to have_content('Log in')
+  end
+
+  # Rainy day cases for creating a user
+  scenario 'invalid inputs (no email)' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+    expect(page).to have_content('Log in')
+  end
+
+  scenario 'invalid inputs (no password)' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password_confirmation', with: 'test'
+    click_on 'Create Account'
+    # Once the user is created, if there is an error the page will reset. So we are checking if the page was reset
+    expect(page).to have_content('Sign up')
+  end
+
+  scenario 'invalid inputs (no password confermation)' do
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    click_on 'Create Account'
+    # Once the user is created, if there is an error the page will reset. So we are checking if the page was reset
+    expect(page).to have_content('Sign up')
+  end
+
+end
+
+# loging in
+RSpec.describe 'Loging in user', type: :feature do
+
+  # Sunny Day cases
+  scenario 'valid login for a non-officer' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    click_on 'Create Account'
+
+    # Actuall test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    expect(page).not_to have_content('View All Points')
+    click_on 'Logout'
+  end
+
+  scenario 'valid login for a officer' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    expect(page).to have_content('View All Points')
+    click_on 'Logout'
+  end
+
+  # Rainy day
+  scenario 'invalid inputs (no email)' do
+    visit root_path
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    expect(page).to have_content('Log in')
+  end
+
+  scenario 'invalid inputs (no password)' do
+    visit root_path
+    fill_in 'email', with: 'test@gmail.com'
+    click_on 'Login'
+    expect(page).to have_content('Log in')
+  end
+
+  scenario 'invalid input (wrong password)' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    click_on 'Create Account'
+
+    # Actuall test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'password'
+    click_on 'Login'
+    expect(page).not_to have_content('Login')
+  end
+
+end
+
+# Viewing users
+RSpec.describe 'Viewing users', type: :feature do
+  scenario 'with an officer account' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    visit portal_view_members_path
+    expect(page).to have_content('Users')
+  end
+end
+
+# Editing User
+# RSpec.describe 'Editinging users', type: :feature do
+#   scenario 'with an officer account' do
+#     visit root_path
+#     # Creating a user for the test to use
+#     click_on 'Sign up'
+#     fill_in 'user_firstName', with: 'first'
+#     fill_in 'user_lastName', with: 'last'
+#     fill_in 'user_email', with: 'test@gmail.com'
+#     fill_in 'user_password', with: 'test'
+#     fill_in 'user_password_confirmation', with: 'test'
+#     check 'user_isOfficer'
+#     click_on 'Create Account'
+
+#     # Actual test
+#     fill_in 'email', with: 'test@gmail.com'
+#     fill_in 'password', with: 'test'
+#     click_on 'Login'
+#     visit portal_view_members_path
+#     click_on 'Edit'
+#     expect(page).to have_content('Edit User')
+#   end
+# end
+
+# Deleting user
+RSpec.describe 'Deletinging users', type: :feature do
+  scenario 'with an officer account' do
+    visit root_path
+    # Creating a user for the test to use
+    click_on 'Sign up'
+    fill_in 'user_firstName', with: 'first'
+    fill_in 'user_lastName', with: 'last'
+    fill_in 'user_email', with: 'test@gmail.com'
+    fill_in 'user_password', with: 'test'
+    fill_in 'user_password_confirmation', with: 'test'
+    check 'user_isOfficer'
+    click_on 'Create Account'
+
+    # Actual test
+    fill_in 'email', with: 'test@gmail.com'
+    fill_in 'password', with: 'test'
+    click_on 'Login'
+    visit portal_view_members_path
+    click_on 'Delete'
+    expect(page).not_to have_content('Delete')
   end
 end
