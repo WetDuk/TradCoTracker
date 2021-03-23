0386f67df4f140342972567c26e9a15d41f73da2
Dak.rich <dak.rich@tamu.edu>
2021-03-22 19:54:02 -0500

    update

--- TradCoTracker/db/migrate/20210321211015_add_password_digest_to_users.rb
+++ TradCoTracker/db/migrate/20210321211015_add_password_digest_to_users.rb
@@ -1,5 +1,5 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[6.1]
   def change
-    add_column :users, :password_digest, :string
+    change_column :table_name, :column_name, :date
   end
 end
