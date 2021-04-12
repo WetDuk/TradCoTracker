2045566e2a1f0e392cbb18d2f67ce2b83a601d8d
Adam Armendariz <armenaa652@gmail.com>
2021-04-12 04:41:56 -0500

    rubocop changes

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,27 +6,29 @@ class Search < ApplicationRecord
     users = User.where(['firstName LIKE ?', "%#{username}%"]) if username.present?
     users = User.where(['email LIKE ?', "%#{email}%"]) if email.present?
     array = []
-    if (min_points.present? || max_points.present?)
+    if min_points.present? || max_points.present?
       puts 'min_points or max_points is present'
-      for user in users do
+      users.each do |user|
         business_sum = Point.where(eventType: 'Business', userID: user.id).sum(:pointAmount)
         speaking_sum = Point.where(eventType: 'Speaking', userID: user.id).sum(:pointAmount)
         total = business_sum + speaking_sum
-        if (total > min_points && max_points > total)
-          puts user.firstName
-          dict = {"firstName" => user.firstName, "lastName" => user.lastName, "business" => business_sum, "speaking" => speaking_sum}
-          array << dict
-        end
+        next unless total > min_points && max_points > total
+
+        puts user.firstName
+        dict = { 'firstName' => user.firstName, 'lastName' => user.lastName,
+                 'business' => business_sum, 'speaking' => speaking_sum }
+        array << dict
       end
     else
       puts 'not min_points or max_points is present'
-      for user in users do
+      users.each do |user|
         business_sum = Point.where(eventType: 'Business', userID: user.id).sum(:pointAmount)
         speaking_sum = Point.where(eventType: 'Speaking', userID: user.id).sum(:pointAmount)
-        dict = {"firstName" => user.firstName, "lastName" => user.lastName, "business" => business_sum, "speaking" => speaking_sum}
+        dict = { 'firstName' => user.firstName, 'lastName' => user.lastName,
+                 'business' => business_sum, 'speaking' => speaking_sum }
         array << dict
       end
     end
-      array
+    array
   end
 end
