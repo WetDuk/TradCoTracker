2ef6eae18332fe984a6ffe1e24630e35a0b0b0c7
Adam Armendariz <armenaa652@gmail.com>
2021-04-11 21:21:30 -0500

    export stuff

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
 class Point < ApplicationRecord
+
+    def self.to_csv
+        attributes = %w{Name Event Type Points Date Comments}
+        CSV.generate(headers: true) do |csv|
+            csv << attributes
+
+            all.each do |point|
+                # csv << points.attributes.values_at(*attributes)
+                currUser = User.find(point.userID)
+                csv << [
+                    currUser.email,
+                    point.eventName,
+                    point.eventType,
+                    point.pointAmount,
+                    point.submissionDate,
+                    point.comments
+                ]
+            end
+        end
+    end
 end
