ae7b7b340e6c1143de7bda923ea8079a865cf155
Adam Armendariz <armenaa652@gmail.com>
2021-04-26 18:53:42 -0500

    announcement validation

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Announcement < ApplicationRecord
+  validates_presence_of :header
+  validates_presence_of :content
+  validates_presence_of :announceDate
 end
